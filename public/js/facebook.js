d80e3949b9c2c6b79267f8566bf054e4860714c0
oswaldo orellana vasquez <oswaldo.orellana.v@gmail.com>
2022-03-24 06:36:54 -0400

    pucha

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -59,24 +59,25 @@ $('#facebookLogin').click(function(event){
     if(error.code === 'auth/account-exists-with-different-credential'){
         var pendingCred = error.credential;
         var email = error.email;
-        auth.fetchSignInMethodsForEmail(email).then(function(methods) {
-        if (methods[0] === 'password') {
-            var password = promptUserForPassword();
-            auth.signInWithEmailAndPassword(email, password).then(function(result) {
-            return result.user.linkWithCredential(pendingCred);
-        }).then(function() {
-          goToApp();
+        firebase.auth.fetchSignInMethodsForEmail(email).then(function(methods) {
+            if (methods[0] === 'password') {
+                var password = promptUserForPassword();
+                firebase.auth.signInWithEmailAndPassword(email, password).then(function(result) {
+                return result.user.linkWithCredential(pendingCred);
+            }).then(function() {
+
+                console.log("método 0");
+            });
+                return;
+            }
+            var provider = getProviderForProviderId(methods[0]);
+            firebase.auth.signInWithPopup(provider).then(function(result) {
+                result.user.linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
+                // Facebook account successfully linked to the existing Firebase user.
+                    console.log("logged", usercred);
+                });
+            });
         });
-        return;
-      }
-      var provider = getProviderForProviderId(methods[0]);
-      auth.signInWithPopup(provider).then(function(result) {
-        result.user.linkAndRetrieveDataWithCredential(pendingCred).then(function(usercred) {
-          // Facebook account successfully linked to the existing Firebase user.
-            console.log("logged", usercred);
-        });
-      });
-    });
     }
   });
 
